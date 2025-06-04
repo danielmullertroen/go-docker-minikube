74e327dd460952604501e9179e7f5395d3c76c58
danielmullertroen <daniel.muller.troen@redcross.no>
2025-06-04 10:59:48 +0200

    some style tweaks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { RegisterForm } from './components/RegisterForm';
 import { UserDetails } from './components/UserDetails';
+import logo from './assets/react.svg'; // or your own logo
 import './App.css';
 
 function App() {
     return (
         <div className="App">
+            <img src={logo} alt="Logo" style={{ width: 64, display: 'block', margin: '0 auto 1.5rem' }} />
             <h1>User Management System</h1>
             <RegisterForm />
             <hr />
