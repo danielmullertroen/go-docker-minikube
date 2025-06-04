745b69323b632ca2c1e338bbbacb67448a489c99
danielmullertroen <daniel.muller.troen@redcross.no>
2025-06-04 14:51:10 +0200

    add /frontend base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,27 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/frontend',
   server: {
     host: true, // This enables 0.0.0.0
     port: 5173,
+    allowedHosts: [
+      'aid-dev.skryter.no',
+      'localhost',
+      '127.0.0.1'
+    ],
     strictPort: true,
     watch: {
       usePolling: true
     }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'aid-dev.skryter.no',
+      'localhost',
+      '127.0.0.1'
+    ]
   }
 })
